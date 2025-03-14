1e75137c60538383e6a26fb42753b1b6eb6b3243
Riccardo Corrazzini <pacoman2006@gmail.com>
2025-03-14 10:47:57 +0100

    aggiunta calendario_settimanale.php

---
+++ fil/calendario_settimanale.php
@@ -0,0 +1,17 @@
+
+
+<?php
+    print("
+
+    <html>
+        <head>
+            <title>calendario</title>
+        </head>
+
+        <body>
+            <h1 style="text-align: center;"></h1>
+        </body>
+    </html>
+
+    ");
+?>
