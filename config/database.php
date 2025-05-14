91e0bf3a5755092879b63fba20be5c6fbdc892b3
samer014 <135011612+samer014@users.noreply.github.com>
2025-05-14 23:59:31 +0100

    cambio nome

---
+++ config/database.php
@@ -0,0 +1,35 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $conn;
+    
+    private function __construct() {
+        $host = "localhost";
+        $db_name = "dbprenotazioniaule";
+        $username = "root";
+        $password = "";
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$host};dbname={$db_name}", 
+                $username, 
+                $password,
+                array(PDO::ATTR_PERSISTENT => true)
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            throw new Exception("Errore di connessione: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
